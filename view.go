a26e82bd573a919b0aff04d38292ff0f101365f0
indiedev02 <dev.kunalshar0@gmail.com>
2026-04-24 19:51:33 +0530

    updated create view

--- view.go
+++ view.go
@@ -17,9 +17,9 @@ func (m model) View() tea.View {
 			s += "\n\n"
 		}
 	case createView:
-		s += "Task Title\n:"
+		s += "Task Title\n"
 		s += m.titleField.View() + "\n"
-		s += "Task Description (optional)\n:"
+		s += "Task Description (optional)\n"
 		s += m.bodyField.View() + "\n"
 	}
 	s += "q, ctrl+c->quit\na->new task"
